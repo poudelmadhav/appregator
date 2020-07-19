d98a5be0979a8fa3c8a040f80f87f5da952115ed
Madhav Paudel <poudelmadhav143@gmail.com>
2020-07-19 16:38:59 +0545

    unnecessary import removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:appregator/screens/news_list.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() => runApp(App());
 
