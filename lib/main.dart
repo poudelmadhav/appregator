b881e57b1fa5744301a242d869f0213122efc5d7
Madhav Paudel <poudelmadhav143@gmail.com>
2020-07-19 18:06:45 +0545

    recreate app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:appregator/screens/news_list.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(App());
+void main() => runApp(MyApp());
 
-class App extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
